87e9f66f45f0e7434349e30c35f2aeb5f3d84a24
Crazy Pioneer <35123708+developer1031@users.noreply.github.com>
2024-03-14 09:45:39 +0900

    gamma 1.1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,18 +1,20 @@
 export const PERSIST_KEY = '@kizuner/persist-key';
 export const USER_TOKEN_KEY = '@kizuner/user-token-key';
 
-export const SHARE_URL = 'https://kizuner-share.inapps.technology'; // staging
-// export const SHARE_URL = 'https://share.kizuner.com'; // prod
-
-// export const SOCKET_HOST = 'https://kizuner-st.inapps.technology/'; // staging
-export const SOCKET_HOST = 'https://source.kizuner.com/'; // prod
-
-export const API_URL = 'http://192.168.110.58:8000/api';
-// export const API_URL = 'https://kizuner-st.inapps.technology/api';
+// prod
+// export const SHARE_URL = 'https://share.kizuner.com';
+// export const SOCKET_HOST = 'https://source.kizuner.com/';
 // export const API_URL = 'https://source.kizuner.com/api';
+// export const STRIPE_KEY = 'pk_live_G7vpDABPNy85jL0JspOMOzg400mI6nFSan';
 
-export const STRIPE_KEY = 'pk_test_b9I41rv7fJZDnHyRxMILK5En00uDwq7mBN'; // staging
-// export const STRIPE_KEY = 'pk_live_G7vpDABPNy85jL0JspOMOzg400mI6nFSan'; // prod
+// staging
+export const SHARE_URL = 'https://kizuner-share.inapps.technology';
+export const SOCKET_HOST = 'https://kizuner-st.inapps.technology/';
+export const STRIPE_KEY = 'pk_test_b9I41rv7fJZDnHyRxMILK5En00uDwq7mBN';
+export const API_URL = 'https://kizuner-st.inapps.technology/api';
+
+// localhost
+// export const API_URL = 'http://192.168.110.58:8000/api';
 
 export const CONTACT_US = 'https://kizuner.com/contact-us/';
 
